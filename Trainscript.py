647a3858680bb5957c76d9f7cb4aa492a4889874
LOYINuts <yushiko0510@gmail.com>
2025-03-02 15:39:51 +0800

    更改优化器为RMSprop

--- Trainscript.py
+++ Trainscript.py
@@ -79,7 +79,9 @@ def main():
         drop_out=conf.drop_prob,
     )
     model = model.to(device=conf.device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=conf.lr)
+    optimizer = torch.optim.RMSprop(
+        model.parameters(), lr=conf.lr, alpha=0.99, eps=1e-8
+    )
     if os.path.exists(model_path) is True:
         print("Loading existing model state_dict......")
         checkpoint = torch.load(model_path, map_location=conf.device, weights_only=True)
