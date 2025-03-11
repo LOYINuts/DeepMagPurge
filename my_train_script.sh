2bcc08880cd2973e660c3d89d647f98ee027dbe3
LOYINuts <yushiko0510@gmail.com>
2025-03-11 22:49:16 +0800

    修改训练脚本

--- my_train_script.sh
+++ my_train_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/home/lys/gh/envs/bioenv/bin/python ./other/art_simulate_genomes.py > train_log.log 2>&1;
-/home/lys/gh/envs/bioenv/bin/python ./other/concat_all_fq.py >> train_log.log 2>&1;
-/home/lys/gh/envs/bioenv/bin/python Trainscript.py >> train_log.log 2>&1;
+/home/lys/gh/envs/bioenv/bin/python ./other/art_simulate_genomes.py;
+/home/lys/gh/envs/bioenv/bin/python ./other/concat_all_fq.py;
+/home/lys/gh/envs/bioenv/bin/python Trainscript.py;
