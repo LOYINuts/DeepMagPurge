baba02192cf9d973d35821ce4be6dc386baa724c
LOYINuts <yushiko0510@gmail.com>
2025-03-12 20:52:49 +0800

    脚本功能分开

---
+++ art_simulate.sh
@@ -0,0 +1,2 @@
+/home/lys/gh/envs/bioenv/bin/python ./other/art_simulate_genomes.py;
+/home/lys/gh/envs/bioenv/bin/python ./other/concat_all_fq.py;
--- my_train_script.sh
+++ my_train_script.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-/home/lys/gh/envs/bioenv/bin/python ./other/art_simulate_genomes.py;
-/home/lys/gh/envs/bioenv/bin/python ./other/concat_all_fq.py;
 /home/lys/gh/envs/bioenv/bin/python Trainscript.py;
