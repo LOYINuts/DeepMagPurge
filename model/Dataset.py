e1bc9532ea3c0118431d4483b274be69e56c6289
LOYINuts <yushiko0510@gmail.com>
2025-03-12 23:41:05 +0800

    彻底取消多进程读取

--- model/Dataset.py
+++ model/Dataset.py
@@ -1,51 +1,8 @@
 from torch.utils.data.dataset import Dataset
-from utils import DataProcess, config
+from utils import DataProcess
 from Bio import SeqIO
 from tqdm import tqdm
 import torch
-import multiprocessing as mp
-
-
-def process_batch(args):
-    batch_records, k, word2idx, max_len, trim = args
-    batch_tensors = []
-    batch_labels = []
-    for rec in batch_records:
-        seq, label_id = Read_Parser(rec)
-        tensor = DataProcess.seq2kmer(seq, k, word2idx, max_len, trim)
-        batch_tensors.append(tensor)
-        batch_labels.append(label_id)
-    return batch_tensors, batch_labels
-
-
-def read_file2data_mp(
-    filepath: str, k: int, word2idx: dict, max_len: int, mode: str, batch_size=1000
-):
-    mp.set_start_method("spawn")
-    trim = mode == "train"
-    with open(filepath, "r") as handle:
-        records = list(SeqIO.parse(handle, "fasta"))
-    batches = [records[i : i + batch_size] for i in range(0, len(records), batch_size)]
-
-    with mp.Pool(processes=4) as pool:
-        # 使用imap+进度条代替map
-        results = list(
-            tqdm(
-                pool.imap(
-                    process_batch,
-                    [(batch, k, word2idx, max_len, trim) for batch in batches],
-                ),
-                total=len(batches),
-                desc="Processing batches",
-            )
-        )
-
-    DataTensor = []
-    Labels = []
-    for tensors, labels in results:
-        DataTensor.extend(tensors)
-        Labels.extend(labels)
-    return DataTensor, Labels
 
 
 def read_file2data(filepath: str, k: int, word2idx: dict, max_len: int, mode: str):
